8acb2182c0ad1dfdf64c1c8ac9d818b0dce2deb7
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-16 23:23:32 +0300

    Add route to delete a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -31,4 +31,22 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
