5051f1ed576d5dc1cb05f0d97b69703e0b6f573e
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-16 23:23:06 +0300

    Add route to update a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,4 +13,22 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
