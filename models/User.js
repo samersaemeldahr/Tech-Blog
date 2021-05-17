448b8543f94ce7e39261cd9d7a0e31c5a1d209dd
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-17 22:42:33 +0300

    Edit password validation

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const sequelize = require('../config/config');
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/config');
 
 class User extends Model {
   checkPassword(loginPw) {
